affcc03a64e089f5dbd3a056260ceb0a1d989333
AnggaChelsea <muhidabdul168@gmail.com>
2020-12-20 15:07:51 +0700

    add games.ts

--- src/app/components/games/games.component.ts
+++ src/app/components/games/games.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from "@angular/router";
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { TournamentService } from '../../shared/services/tournament/tournament.service';
+import { Tournament } from '../../shared/models/tournament';
+
 
 @Component({
   selector: 'app-games',
@@ -7,9 +15,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamesComponent implements OnInit {
 
-  constructor() { }
+ tournaments: any;
+ currentTournament = null;
+ currentIndex = -1;
+ name = '';
+
+  constructor(private tournamentService:TournamentService) {
+  }
 
   ngOnInit(): void {
+     this.readTournament();
   }
+  readTournament(): void {
+    this.tournamentService.readAll()
+      .subscribe(
+        data => {
+          this.tournaments = data;
+          console.log(this.tournaments);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  refresh(): void {
+    this.readTournament();
+    this.currentTournament = null;
+    this.currentIndex = -1;
+  }
+
+  setCurrentProduct(tournament:any, index:any): void {
+   this.currentTournament = tournament;
+   this.currentIndex = index;
+ }
+
+ searchByName(): void {
+    this.tournamentService.searchByName(this.name)
+      .subscribe(
+        data => {
+          this.tournaments = data;
+          console.log(this.tournaments);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+
+
 
 }
