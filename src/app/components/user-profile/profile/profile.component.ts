3bbbb76e6a48c2728980f400a8e0e3f68e2ba3ae
bloodybuttock <jurukuncicpm@gmail.com>
2021-01-08 21:48:54 +0700

    udatessss

--- src/app/components/user-profile/profile/profile.component.ts
+++ src/app/components/user-profile/profile/profile.component.ts
@@ -7,26 +7,27 @@ import { ProfileService } from 'src/app/shared/services/profile/profile.service'
   styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
-  subDistrict:any|string 
-  birthDate:Date | undefined
-  phoneNumber:number | undefined;
-  fullname:string | undefined
-  _groupId:string | undefined
-  _tournamentId:string | undefined
-  picture:string | undefined
-  path:any
-  constructor(private profileService: ProfileService) { }
+  subDistrict: any | string;
+  birthDate: Date | undefined;
+  phoneNumber: number | undefined;
+  fullname: string | undefined;
+  _groupId: string | undefined;
+  _tournamentId: string | undefined;
+  picture: string | undefined;
+  path: any;
+  constructor(private profileService: ProfileService) {}
 
   ngOnInit(): void {
     this.profileService.getProfile().subscribe((res) => {
-      this.path = "http://localhost:5000/"
-      this.picture = res.picture
-      this.subDistrict = res.subDistrict
-      this.birthDate = res.birthDate
-      this.phoneNumber = res.phoneNumber
-      this.fullname = res.fullname
-      this._groupId = res._groupId
-      this._tournamentId = res._tournamentId
-    })
+      // this.path = "http://localhost:5000/"
+      this.path = 'https://git.heroku.com/radiant-reef-49263.git/';
+      this.picture = res.picture;
+      this.subDistrict = res.subDistrict;
+      this.birthDate = res.birthDate;
+      this.phoneNumber = res.phoneNumber;
+      this.fullname = res.fullname;
+      this._groupId = res._groupId;
+      this._tournamentId = res._tournamentId;
+    });
   }
 }
