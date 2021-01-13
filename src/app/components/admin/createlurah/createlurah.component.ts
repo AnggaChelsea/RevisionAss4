78f9c0c88838cb3cedda177bf815101545ede4f3
naufalibn24 <72061940+naufalibn24@users.noreply.github.com>
2021-01-13 17:02:02 +0700

    assign part

--- src/app/components/admin/createlurah/createlurah.component.ts
+++ src/app/components/admin/createlurah/createlurah.component.ts
@@ -1,6 +1,9 @@
 import { AuthService } from './../../../shared/services/auth/auth.service';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { AdminService } from 'src/app/shared/services/admin/admin.service';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { Assign } from 'src/app/shared/models/Admin';
+
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
 
@@ -11,10 +14,25 @@ import Swal from 'sweetalert2';
 })
 export class CreatelurahComponent implements OnInit {
 
-  
-  constructor() { }
+  constructor(private adminService: AdminService,public fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
-
+  asignForm:any = new FormGroup({
+    _id: new FormControl(''),
+    subDistrict: new FormControl('')
+  })
+  Assign():void {
+    const assign: Assign ={
+      _id : this.asignForm.get('_id').value,
+      subDistrict: this.asignForm.get('subDistrict').value
+    }
+    this.adminService.assign(assign).subscribe(response => {
+      Swal.fire({
+        icon: 'success',
+        title: 'Assign Success',
+        text: `Assign to Headchief`,
+      })
+    })
+  }
 }
